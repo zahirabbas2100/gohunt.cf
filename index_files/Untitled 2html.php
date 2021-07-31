448382d0d4f332dfee0d4d45b178e5d7bdb42744
zahirabbas2100 <75905280+zahirabbas2100@users.noreply.github.com>
2021-07-31 05:35:00 -0700

    Add files via upload

---
+++ index_files/Untitled 2html.php
@@ -0,0 +1,54 @@
+<html>
+
+
+<body>
+
+
+
+<head>
+
+                         
+<div class="container">
+<div class="left"></div>
+<div class="center"></div>
+<div class="right"></div>
+             .container{width: 150px;
+             height:150;
+             background:black;
+             .left{flot:left
+             }
+             .center{display:inline-block
+             .right{float:right}
+             
+</div> 
+
+</head>
+</body>
+</html>
+
+
+
+<div style="width: 100%;height: 899px;background-color: yellowgreen;margin-top: 5px;;">
+
+
+
+
+<div style="width: 100%;height: 899px;background-color: yellowgreen;margin-top: 5px;;">
+
+          
+<div style="width: 400px;height: 300px;background-color: aliceblue;position: absolute;top: 50%;left: 50%;transform: translate;(-50%,-50%)"></div>
+ 
+</div>
+
+            
+            </div><!-- /.story-section -->
+                         
+            </div>
+       </div>
+
+</div>
+
+             </div>
+        
+       
+            
